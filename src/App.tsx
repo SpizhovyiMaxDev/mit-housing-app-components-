4ecc49a568c535014d26ca5c8f5f86b27c9c2a4d
samplevich <samplesampleovich@gmail.com>
2025-09-04 14:32:41 -0700

    Components setup

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,27 @@ import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 
 function App() {
-  const position: [number, number] = [51.505, -0.09];
+  const position: [number, number] = [56.1304, -106.3468];
 
   return (
-    <MapContainer
-      style={{ height: "100vh", width: "100%" }}
-      center={position}
-      zoom={13}
-      scrollWheelZoom={false}
-    >
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <Marker position={position}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
-    </MapContainer>
+    <div className="map-wrapper">
+      <MapContainer
+        className="map"
+        center={position}
+        zoom={4}
+        scrollWheelZoom={false}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={position}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </div>
   );
 }
 
